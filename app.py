c4670f69b753154c48b47433321e339fa0558ad7
jolenezheng <jolenezheng18@gmail.com>
2021-03-12 18:14:03 -0500

    changed styles

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -6,6 +8,7 @@ import numpy as np
 from dash.dependencies import Output, Input
 import plotly.express as px
 import plotly.graph_objects as go
+import datetime as dt
 
 from textwrap import dedent
 
@@ -15,6 +18,11 @@ data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
 data.sort_values("Date", inplace=True)
 
 df_mort = pd.read_csv('https://raw.githubusercontent.com/ccodwg/Covid19Canada/master/timeseries_hr/mortality_timeseries_hr.csv')
+df_mort["date_death_report"] = pd.to_datetime(df_mort["date_death_report"], format="%d-%m-%Y")
+
+weather_base_url = 'https://dd.weather.gc.ca/climate/observations/daily/csv/'
+
+df_mob = pd.read_csv('https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv')
 
 
 external_stylesheets = [
@@ -93,39 +101,89 @@ app.layout = html.Div(
             children=[
                 html.Div(
                     children=[
-                        html.Div(children="Region", className="menu-title"),
-                        dcc.Dropdown(
-                            id='region-dropdown',
-                            className='dropdown',
-                            options=[{'label':name, 'value':name} for name in names],
-                            value = list(fnameDict.keys())[0]
-                        ),
-                    ]
-                ),
-                html.Div(
-                    children=[
-                        html.Div(children="Sub-Region", className="menu-title"),
-                        dcc.Dropdown(
-                            id='subregion-dropdown',
-                            className='dropdown',
-                            value = 'None'
-                        ),
-                    ]
-                ),
-                html.Div(
-                    children=[
+                        html.Div(children="Daily Mortality", className="options-header"),
                         html.Div(
-                            children="Date Range",
-                            className="menu-title"
-                            ),
-                        dcc.DatePickerRange(
-                            id="date-range",
-                            min_date_allowed=data.Date.min().date(),
-                            max_date_allowed=data.Date.max().date(),
-                            start_date=data.Date.min().date(),
-                            end_date=data.Date.max().date(),
+                            children=[
+                                html.Div(children="Region", className="dropdown-title"),
+                                dcc.Dropdown(
+                                    id='region-dropdown',
+                                    className='dropdown',
+                                    options=[{'label':name, 'value':name} for name in names],
+                                    value = list(fnameDict.keys())[0]
+                                ),
+                            ]
                         ),
-                    ]
+                        html.Div(
+                            children=[
+                                html.Div(children="Sub-Region", className="dropdown-title"),
+                                dcc.Dropdown(
+                                    id='subregion-dropdown',
+                                    className='dropdown',
+                                    value = 'None'
+                                ),
+                            ]
+                        ),
+                        html.Div(
+                            children=[
+                                html.Div(
+                                    children="Date Range",
+                                    className="dropdown-title"
+                                    ),
+                                dcc.DatePickerRange(
+                                    id="date-range",
+                                    min_date_allowed=df_mort.date_death_report.min().date(),
+                                    max_date_allowed=df_mort.date_death_report.max().date(),
+                                    start_date=df_mort.date_death_report.min().date(),
+                                    end_date=df_mort.date_death_report.max().date(),
+                                ),
+                            ]
+                        ),
+                    ],
+                    className="options-card",
+                ),
+                html.Div(
+                    children=[
+                        html.Div(children="Weather Data", className="options-header"),
+                        html.Div(
+                            children=[
+                                html.Div(children="Province", className="dropdown-title"),
+                                dcc.Dropdown(
+                                    id='weather-prov-dropdown',
+                                    className='dropdown',
+                                    options=[{'label':name, 'value':name} for name in names],
+                                    value = list(fnameDict.keys())[0]
+                                ),
+                            ]
+                        ),
+                        html.Div(
+                            children=[
+                                html.Div(children="Station ID", className="dropdown-title"),
+                                dcc.Dropdown(
+                                    id='weather-id-dropdown',
+                                    className='dropdown',
+                                    options=[{'label': '3031092', 'value': '3031092'}],
+                                    # options=[{'label':name, 'value':name} for name in names],
+                                    # value = list(fnameDict.keys())[0]
+                                ),
+                            ]
+                        ),
+                        html.Div(
+                            children=[
+                                html.Div(
+                                    children="Date Range",
+                                    className="dropdown-title"
+                                    ),
+                                dcc.DatePickerRange(
+                                    id="weather-date-range",
+                                    min_date_allowed=df_mort.date_death_report.min().date(),
+                                    max_date_allowed=df_mort.date_death_report.max().date(),
+                                    start_date=df_mort.date_death_report.min().date(),
+                                    end_date=df_mort.date_death_report.max().date(),
+                                ),
+                            ]
+                        ),
+                    ],
+                    className="options-card",
                 ),
             ],
             className="left-column columns",
@@ -140,9 +198,17 @@ app.layout = html.Div(
                 # ),
                 html.Div(
                     children=[
+                        html.Div(children="Daily Mortality Graph", className="options-header"),
                         html.Div(
                             children=dcc.Graph(
                                 id="covid-chart", config={"displayModeBar": False},
+                            ),
+                            className="card",
+                        ),
+                        html.Div(children="Weather Graph", className="options-header"),
+                        html.Div(
+                            children=dcc.Graph(
+                                id="weather-chart", config={"displayModeBar": False},
                             ),
                             className="card",
                         ),
@@ -163,42 +229,102 @@ def update_date_dropdown(name):
     return [{'label': i, 'value': i} for i in fnameDict[name]]
 
 
+# ============== MORTALITY GRAPH ==============
 @app.callback(
     Output("covid-chart", "figure"),
     [
         Input("region-dropdown", "value"),
         Input("subregion-dropdown", "value"),
+        Input("date-range", "start_date"),
+        Input("date-range", "end_date"),
     ],
 )
-def update_covid_chart(name, region):
-    print(name)
-    print(region)
-    
+def update_covid_chart(name, region, start_date, end_date):
+    print(start_date)
+    print(end_date)
     if (name == "Newfoundland and Labrador"):
         name = "NL"
     elif (name == "British Columbia"):
         name = "BC"
 
-    fig = px.line(df_mort, x = date(name, region), y = r_avg(name, region))
+    fig = px.line(df_mort, x = date(name, region, start_date, end_date), y = r_avg(name, region, start_date, end_date))
 
-    # fig = go.Figure(data=go.Scatter(x=x, y=y))
     fig.update_layout(title='Daily Reported Deaths in ' + region + ', ' + name,
                    xaxis_title='Date',
-                   yaxis_title='Daily Mortality(7-day Rolling Average')
+                   yaxis_title='Daily Mortality (7-day Rolling Average)')
+
+    return fig
+
+
+# ============== WEATHER GRAPH ==============
+@app.callback(
+    Output("weather-chart", "figure"),
+    [
+        Input("weather-prov-dropdown", "value"),
+        Input("weather-id-dropdown", "value"),
+        Input("weather-date-range", "start_date"),
+        Input("weather-date-range", "end_date"),
+    ],
+)
+def update_weather_chart(prov, station_id, start_date, end_date):
+    if (prov == "Newfoundland and Labrador"):
+        namprove = "NL"
+    elif (prov == "British Columbia"):
+        prov = "BC"
+
+    prov = "AB"
+    station_id = "3031092";
+    date_rang = "2016-12"
+    # date_rang = date(2016,12,1).strftime("%Y-%m")
+
+
+    # target_url = weather_base_url + prov + '/climate_daily_' + prov+ '_' +station_id+ '_' +date_rang+ '_P1D.csv'
+    # weat_data =  pd.read_csv(target_url, encoding='Latin-1')
+
+    # weat_city = weat_data['Mean Temp (•C)']
+    # date_city = weat_data['Date/Time']
+
+    x = np.arange(10)
+
+    fig = px.line(df_mort, x = x, y = x*x*2)
+    # fig = px.line(df_mort, x = date_city, y = weat_city)
+
+    # fig = go.Figure(data=go.Scatter(x=date_city, y=weat_city))
+    # fig = px.line(df_mort, x = date(prov, region, start_date, end_date), y = r_avg(prov, region, start_date, end_date))
+
+    fig.update_layout(title='Daily Reported Temperature in ' + prov,
+                   xaxis_title='Date',
+                   yaxis_title='Mean Temperature')
 
     return fig
 
 
 
-def date(province_name, region_name):
+def date(province_name, region_name, start_date, end_date):
+
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+
+    filtered_df = df_mort[df_mort.date_death_report.between(
+        dt.datetime.strftime(start_date, "%d-%m-%Y"),
+        dt.datetime.strftime(end_date, "%d-%m-%Y")
+    )]
     
-    df_province = df_mort[df_mort.province == province_name]
+    df_province = filtered_df[filtered_df.province == province_name]
     return df_province.date_death_report[df_province.health_region == region_name]
     
 
-def r_avg(province_name, region_name):
+def r_avg(province_name, region_name, start_date, end_date):
+
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+
+    filtered_df = df_mort[df_mort.date_death_report.between(
+        dt.datetime.strftime(start_date, "%d-%m-%Y"),
+        dt.datetime.strftime(end_date, "%d-%m-%Y")
+    )]
     
-    df_province = df_mort[df_mort.province == province_name]
+    df_province = filtered_df[filtered_df.province == province_name]
     return df_province.deaths[df_province.health_region == region_name].rolling(window=7).mean()
 
 
